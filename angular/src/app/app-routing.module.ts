29f0a88e1960b70f6922d9de2b4af874d9894078
adnan <adnan.nisar@quicktijarat.com>
2023-10-11 21:09:55 +0500

    e

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './common/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashBoardComponent ,data: { title: 'Dashboard' }},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
