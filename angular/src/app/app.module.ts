dc90229d8a161ff289b34f7f07cab1a8383cfd09
adnan <adnan.nisar@quicktijarat.com>
2023-10-24 20:34:58 +0500

    some routing and side baar

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashBoardComponent } from './common/dash-board.component';
+import { LoginComponent } from './common/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashBoardComponent
+    DashBoardComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
